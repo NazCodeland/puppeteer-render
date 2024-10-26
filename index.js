f78cc2bc2f4727e5dd0060ac7ae840631757fa3e
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-10-26 13:32:59 -0400

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const handleScreenshot = require("./screenshot");
+const { handleScreenshot } = require("./screenshot");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
