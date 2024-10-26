6820cee21b31a041a11085a4380a9c08d21ed26e
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-10-26 13:22:37 -0400

    fix

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
+const handleScreenshot = require("./screenshot");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+app.get("/scrape", async (req, res) => {
+  const options = {
+    url: 'https%3A%2F%2Ftailwindcss.com',
+    viewport: {
+      width: 320,
+      height: 568,
+    },
+    imageHeight: 3900
+  };
+
+  const screenshots = await handleScreenshot(options);
+  res.status(200).json(screenshots);
 });
 
 app.get("/", (req, res) => {
---
+++ screenshot.js
@@ -0,0 +1,51 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+async function launchConfiguredBrowser() {
+    const browser = await puppeteer.launch({
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+        ],
+        executablePath: process.env.NODE_ENV === "production"
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
+            : puppeteer.executablePath(),
+    });
+    return browser;
+}
+async function takeScreenshot(options, page) {
+    if (typeof options.url !== 'string') {
+        throw new Error('No url provided');
+    }
+    await page.goto(options.url, { waitUntil: 'networkidle0' });
+    await page.setViewport(options.viewport);
+    const height = await page.evaluate(() => document.body.scrollHeight);
+    const screenshots = [];
+    for (let offset = 0; offset < height; offset += options.imageHeight) {
+        await page.evaluate((offset) => window.scrollTo(0, offset), offset);
+        const screenshot = await page.screenshot({
+            fullPage: false,
+            clip: {
+                x: 0,
+                y: offset,
+                width: options.viewport.width,
+                height: Math.min(options.imageHeight, height - offset),
+            },
+        });
+        screenshots.push(screenshot);
+    }
+    console.log('Screenshots taken', screenshots);
+    return screenshots;
+}
+export async function handleScreenshot(options) {
+    const browser = await launchConfiguredBrowser();
+    const pages = await browser.pages();
+    const page = pages[0];
+    await page.setViewport(options.viewport);
+    const screenshots = await takeScreenshot(options, page);
+    await browser.close();
+    return screenshots;
+}
+//# sourceMappingURL=screenshot.js.map
