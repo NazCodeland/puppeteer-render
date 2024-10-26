33b2c41c15af58ca1b18eceb9040a828751117cb
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-10-26 15:49:26 -0400

    fix

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
   }
