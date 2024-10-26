57a53bf0e88c96b06ec7fdac55a6515b7c9cfde0
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-10-26 15:42:02 -0400

    fix

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { handleScreenshot } = require("./screenshot");
-const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Enable CORS
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+});
 
 app.get("/scrape", async (req, res) => {
   const rawOptions = '%7B%22url%22%3A%22https%3A%2F%2Ftailwindcss.com%22%2C%22viewport%22%3A%7B%22width%22%3A320%2C%22height%22%3A568%7D%2C%22imageHeight%22%3A4096%7D';
