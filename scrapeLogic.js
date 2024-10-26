f561938502440c657d12c827d3a123bd4984da24
Nazar <113494366+NazCodeland@users.noreply.github.com>
2024-10-26 13:00:18 -0400

    fix

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,28 +17,16 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://nazcodeland.dev/");
 
-    // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    // Grab the text from the element
+    const logStatement = await page.evaluate(() => {
+      const element = document.querySelector('h2.forcedClrAdjust');
+      return element ? element.innerText : '';
+    });
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
     res.send(logStatement);
   } catch (e) {
     console.error(e);
